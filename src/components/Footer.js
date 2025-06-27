9215bf22dc49a6af456cc1687eaa5fa643d98cf8
James White <124373979+white-james@users.noreply.github.com>
2025-06-27 17:03:53 +0100

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="text-center mt-5">
-      <p>&copy; 2025 James White. All rights reserved.</p>
-    </footer>
-  );
-}
